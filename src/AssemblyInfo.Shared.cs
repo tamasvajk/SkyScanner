ef94f3fa6a6e49010786330f0baea6c909456a32
Tamas Vajk <tamas.vajk@gmail.com>
2015-06-12 07:12:25 +0200

    Updating the version number

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
