726d66e575da263cbeed9b8b7c4595a92c68ab50
Tamas Vajk <tamas.vajk@gmail.com>
2015-08-12 18:07:50 +0200

    Version number change to 1.3.0

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
