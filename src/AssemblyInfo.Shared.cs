3c9654fe4336de5759df90ea6e9f148719de6269
Tamas Vajk <tamas.vajk@gmail.com>
2016-07-24 14:24:52 +0200

    Change licence year

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016 Tamas Vajk")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.3.4.0")]
 [assembly: AssemblyFileVersion("1.3.4.0")]
--- src/SkyScanner/Settings/IExecutionStrategy.cs
+++ src/SkyScanner/Settings/IExecutionStrategy.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Tamas Vajk. All Rights Reserved. Licensed under the MIT License. See License.txt in the project root for license information.
+// Copyright (c) 2015-2016 Tamas Vajk. All Rights Reserved. Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
 using System.Threading;
